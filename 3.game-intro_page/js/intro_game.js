9f782c06087ab307487d7e013acf34e10b7c6e2f
code-MIM <2153rud@gmail.com>
2021-06-19 00:15:12 +0900

    Web_Work

--- 3.game-intro_page/js/intro_game.js
+++ 3.game-intro_page/js/intro_game.js
@@ -2,6 +2,7 @@ const triangle = document.querySelectorAll(".triangle");
 const firstWrap = document.querySelector(".firstWrap");
 const secondWrap = document.querySelector(".secondWrap");
 const thirdWrap = document.querySelector(".thirdWrap");
+const forthWrap = document.querySelector(".forthWrap");
 
 // ====================
 
@@ -9,6 +10,16 @@ window.addEventListener("load", function() {
   triangle[0].style.bottom = "250px";
   triangle[0].style.right = "300px";
 })
+
+for (let i = 0; i < triangle.length; i++) {
+  if (i == 2n & i != 0) {
+    triangle[i].style.bottom = "40px";
+    triangle[i].style.left = "60px";
+  } else {
+    triangle[i].style.bottom = "40px";
+    triangle[i].style.right = "60px";
+  }
+}
 
 triangle[0].addEventListener("click", function() {
   firstWrap.style.display = "none";
@@ -18,4 +29,9 @@ triangle[0].addEventListener("click", function() {
 triangle[1].addEventListener("click", function() {
   secondWrap.style.display = "none";
   thirdWrap.style.display = "block";
+})
+
+triangle[2].addEventListener("click", function() {
+  thirdWrap.style.display = "none";
+  forthWrap.style.display = "block";
 })
