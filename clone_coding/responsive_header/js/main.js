4fa014bc7528c15f2aaf411a8c86a2cd33f21378
code-MIM <2153rud@gmail.com>
2021-06-21 23:45:13 +0900

    Web_Work

---
+++ clone_coding/responsive_header/js/main.js
@@ -0,0 +1,9 @@
+const toggleBtn = document.querySelector(".navbar_toggleBtn");
+const menu = document.querySelector(".navbar_menu");
+const link = document.querySelector(".navbar_links");
+
+
+toggleBtn.addEventListener("click", function() {
+  menu.classList.toggle("active");
+  link.classList.toggle("active");
+ })
