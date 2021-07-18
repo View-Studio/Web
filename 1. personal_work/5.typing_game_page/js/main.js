94ed18f0ef2ba02f09947dcd09260603c5273f07
code-MIM <2153rud@gmail.com>
2021-07-18 22:56:00 +0900

    Web_Work

--- 1. personal_work/5.typing_game_page/js/main.js
+++ 1. personal_work/5.typing_game_page/js/main.js
@@ -3,3 +3,8 @@ const wordDisplay = document.querySelector('.word-display');
 const scoreDisplay = document.querySelector('.score-point');
 const timerDisplay = document.querySelector('.timer-point');
 const button = document.querySelector('.start-btn');
+
+
+wordInput.addEventListener('input', function(){
+    console.log(wordInput.value);
+})
