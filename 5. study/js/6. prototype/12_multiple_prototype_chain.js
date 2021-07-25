6dac7001745cb75d48d4000c40e8f34fec7bb76e
code-MIM <2153rud@gmail.com>
2021-07-26 05:03:02 +0900

    Web_Work

--- 5. study/js/6. prototype/12_multiple_prototype_chain.js
+++ 5. study/js/6. prototype/12_multiple_prototype_chain.js
@@ -6,15 +6,15 @@ var Grade = function () {
   this.length = args.length;
 };
 
-var g = new Grade(100 ,80);
-
 Grade.prototype = [];
-g.__proto__ = Grade.prototype
 /*
-g.__proto__ 에 Grade.prototype를 대입해주지 않으면,
+Grade.prototype의 값을 바꾸는 코드를 인스턴스 생성문 보다 앞에 쓰지않으면 
 Grade.prototype이 []로 바뀌면서 Grade.prototype의 __proto__가 Array의 prototype으로 바뀌지만,
 g의 __proto__는 []로 바뀌지 않고 []를 대입하기 전의 객체를 그대로 참조하게 된다.
 */
+
+var g = new Grade(100 ,80);
+
 
 console.log(g);    // Grade(2) [100, 80]
 g.pop();
