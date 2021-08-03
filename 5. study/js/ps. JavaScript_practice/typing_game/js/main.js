0d3051a94c3490ed2dca9c2a78a4d640bc312660
code-MIM <2153rud@gmail.com>
2021-08-03 23:16:27 +0900

    Web_Work

--- 5. study/js/ps. JavaScript_practice/typing_game/js/main.js
+++ 5. study/js/ps. JavaScript_practice/typing_game/js/main.js
@@ -2,3 +2,4 @@ const wordDisplay = document.querySelector('.word-display');
 const timePoint = document.querySelector('.timer-point');
 const scorePoint = document.querySelector('.score-point');
 const button = document.querySelector('.start-btn');
+const button = document.querySelector('.start-btn');
