fb1da2a8e749db3921d52146695a7199f6582b82
code-MIM <2153rud@gmail.com>
2021-08-13 23:20:36 +0900

    Web_Work

--- 5. study/js/7. class/00_static_method_&_prototype_method.js
+++ 5. study/js/7. class/00_static_method_&_prototype_method.js
@@ -4,14 +4,17 @@ var Rectangle = function (width, height) {
 };
 
 
+
 Rectangle.prototype.getArea = function () {
     return this.width * this.height;
 };
 
 
+
 Rectangle.isRectangle = function (instance) {
     return instance instanceof Rectangle && instance.width > 0 && instance.height > 0;
 }
+
 
 
 var rect1 = new Rectangle(3, 4)
