a3525f80bb8e06c3cc546eb45b79fe8946afb4f5
code-MIM <2153rud@gmail.com>
2021-09-26 21:28:22 +0900

    Web_Work

--- 5. study/js/3. this/00_global_space_this.js
+++ 5. study/js/3. this/00_global_space_this.js
@@ -10,6 +10,8 @@ console.log(this === window); //true
 
 
 
+
+
 /* Node.js */
 console.log(this); // { process: { title: 'node', version: 'v10.13.0', …}}
 console.log(global); // { process: { title: 'node', version: 'v10.13.0', …}}
