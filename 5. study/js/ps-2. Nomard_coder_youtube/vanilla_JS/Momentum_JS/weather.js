291877791eb19afe0168dd7f155bfae7a82a69d6
code-MIM <2153rud@gmail.com>
2021-11-16 23:36:28 +0900

    Web_Work

--- 5. study/js/ps-2. Nomard_coder_youtube/vanilla_JS/Momentum_JS/weather.js
+++ 5. study/js/ps-2. Nomard_coder_youtube/vanilla_JS/Momentum_JS/weather.js
@@ -1,12 +1,33 @@
 const COORDS = "coords";
 
-const loadCoords = function() {
-  const loadedCords = localStorage.getItem(COORDS);
+
+const handleGeoSucces = function(position) {
+  console.log(position);
 }
 
 
-const init = function() {
+const handleGeoError = function() {
+  console.log("Error! Can't access geo location.")
+}
 
+
+const askForCoords = function() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+
+const loadCoords = function() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    // getWeather Function
+  }
+}
+
+
+function init() {
+  loadCoords();
 }
 
 init();
